235d10a65e2c72da7f9911839dc4acd4f68a1936
Amrit Pandey <spanit007@gmail.com>
2017-10-03 17:33:31 +0530

    Refactoring failed, reverted back to original structure

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-require('./actions/newMemberAdd').greet();
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'general');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}.
+                Have a look at our #welcome channel
+                for rules and futher informations`);
+  const image = `https://i.imgur.com/lGvt46k.png`;
+  channel.send(image);
+});
 
-client.login('process.env.BOT_TOKEN');
+
+client.login('MzU1MzgxMDUwNjk4MTA0ODM1.DLUItQ.GwwGXgskuoWFAfC50VgE8CqD9zo');
