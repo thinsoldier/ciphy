6a767b4a50e3cdcd4e28adca79ec40a9fd5a726e
Amrit Pandey <spanit007@gmail.com>
2017-10-03 23:42:33 +0530

    Replace general channel with welcome channel in message

--- modules/functions.js
+++ modules/functions.js
@@ -1,10 +1,15 @@
 module.exports = client => {
     client.on('guildMemberAdd', member => {
+
+        const generalChannel = member.guild.channels.find('name', 'general');
+        if (!generalChannel) return;
+
+        const welcomeChannel = member.guild.channels.find('name', 'welcome');
+
         let devRole = member.guild.roles.find('name', 'Developer');
         member.addRole(devRole);
-        const channel = member.guild.channels.find('name', 'general');
-        if (!channel) return;
-        channel.send(`Welcome to the server, ${member}. Have a look at our ${channel} channel for rules and futher informations`);
+        
+        generalChannel.send(`Welcome to the server, ${member}. Have a look at our ${welcomeChannel} channel for rules and futher informations`);
         const image = `https://i.imgur.com/lGvt46k.png`;
         channel.send(image);
       });
