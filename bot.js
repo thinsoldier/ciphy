fdb5b353a59f3d0159578d3d28fd4cefdfa0f270
Amrit Pandey <spanit007@gmail.com>
2017-10-03 19:16:35 +0530

    Added a functionality by which the Developer role is assigned to new member

--- bot.js
+++ bot.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
+  let devRole = member.guild.roles.find('name', 'Developer');
+  member.addRole(devRole);
   const channel = member.guild.channels.find('name', 'general');
   if (!channel) return;
-  channel.send(`Welcome to the server, ${member}.
-                Have a look at our #welcome channel
-                for rules and futher informations`);
+  channel.send(`Welcome to the server, ${member}. Have a look at our ${channel} channel for rules and futher informations`);
   const image = `https://i.imgur.com/lGvt46k.png`;
   channel.send(image);
 });
 
 
-client.login('MzU1MzgxMDUwNjk4MTA0ODM1.DLUItQ.GwwGXgskuoWFAfC50VgE8CqD9zo');
+client.login('process.env.BOT_TOKEN');
