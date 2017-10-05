151c4c35a8538f7059ad2fbe4b3e88580da82d84
Amrit Pandey <spanit007@gmail.com>
2017-10-05 14:29:48 +0530

    Added few comments

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//Greet and set the role to developer for every new member.
 require('./modules/functions')(client);
 
 client.login(process.env.BOT_TOKEN);
