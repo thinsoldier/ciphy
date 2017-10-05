f4eb51a2207b2ac8055ceb0069ad6901da5cd76f
Amrit Pandey <spanit007@gmail.com>
2017-10-05 13:44:07 +0530

    general fix and changed finding name to channel ID

--- modules/functions.js
+++ modules/functions.js
@@ -1,16 +1,16 @@
 module.exports = client => {
     client.on('guildMemberAdd', member => {
 
-        const generalChannel = member.guild.channels.find('name', 'general');
+        const generalChannel = member.guild.channels.find('id', '290833497373802496');
         if (!generalChannel) return;
-
-        const welcomeChannel = member.guild.channels.find('name', 'welcome');
+        //The random numbers are channel ID
+        const welcomeChannel = member.guild.channels.find('id', '363967155634438144');
 
         let devRole = member.guild.roles.find('name', 'Developer');
         member.addRole(devRole);
         
         generalChannel.send(`Welcome to the server, ${member}. Have a look at our ${welcomeChannel} channel for rules and futher informations`);
         const image = `https://i.imgur.com/lGvt46k.png`;
-        channel.send(image);
+        generalChannel.send(image);
       });
 }
