dfce7bb7ee5ace5cb70e164cb6a010c4d347baa2
Amrit Pandey <spanit007@gmail.com>
2017-10-05 16:52:44 +0530

    moved client from discord to commando

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
 const commando = require('discord.js-commando');
-const client = new commando.Client();
+const path = require('path');
+const client = new commando.Client({
+    owner : '274434863711518722'
+});
 
 //Greet and set the role to developer for every new member.
 require('./modules/functions')(client);
+
+client.registry()
+    .registerGroup('ciphy', 'ciphy commands')
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname+'/commands'));
 
 client.login('MzY0MzAzMDUzOTE2MjA5MTUy.DLd91A.VCQ2oe1-Au7551ku2PdZ2x_p5EE');
